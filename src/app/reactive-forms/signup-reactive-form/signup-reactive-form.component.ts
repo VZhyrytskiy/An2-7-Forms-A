8507240168a2bcb1b4dca635d0636b09e2d94e99
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-02-22 13:46:22 +0200

    fix code

--- src/app/reactive-forms/signup-reactive-form/signup-reactive-form.component.ts
+++ src/app/reactive-forms/signup-reactive-form/signup-reactive-form.component.ts
@@ -33,7 +33,7 @@ export class SignupReactiveFormComponent implements OnInit {
       firstName: new FormControl(),
       lastName: new FormControl(),
       email: new FormControl(),
-      sendCatalog: new FormControl(true)
+      sendProducts: new FormControl(true)
     });
   }
 
