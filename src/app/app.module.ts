dffa0d1ba5c1804b177d9e87b925ac8ec6bb73ec
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-09-18 14:01:28 +0300

    async email validator directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { ValidatorsModule } from './validators/validators.module';
+
 import { AppComponent } from './app.component';
 import { SignupFormComponent } from './template-driven-forms/signup-form';
 import { SignupReactiveFormComponent } from './reactive-forms/signup-reactive-form/signup-reactive-form.component';
-import { ServiceLevelDirective } from './validators/service-level.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupFormComponent,
-    SignupReactiveFormComponent,
-    ServiceLevelDirective
+    SignupReactiveFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule
+    HttpModule,
+    ValidatorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
