8602353696c9ada93ea4324b02db53be84379f54
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-02-07 18:50:21 +0200

    fix11

--- src/app/validators/service-level.directive.ts
+++ src/app/validators/service-level.directive.ts
@@ -5,14 +5,15 @@ import { checkServiceLevel } from './custom.validators';
 
 @Directive({
   selector: '[appServiceLevelValidator]',
-  providers: [{
+  providers: [
+    {
       provide: NG_VALIDATORS,
-      useClass: ServiceLevelDirective,
+      useExisting: ServiceLevelDirective,
       multi: true
-  }]
+    }
+  ]
 })
 export class ServiceLevelDirective implements Validator {
-
   validate(c: AbstractControl): { [key: string]: boolean } | null {
     return checkServiceLevel(c, 1, 3);
   }
