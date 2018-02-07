183545b418d890a1cbcc3152814c3444596a5f69
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-02-07 18:19:52 +0200

    fix11

--- src/app/validators/service-level.directive.ts
+++ src/app/validators/service-level.directive.ts
@@ -1,23 +1,19 @@
 import { Directive } from '@angular/core';
 import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
 
+import { checkServiceLevel } from './custom.validators';
+
 @Directive({
   selector: '[appServiceLevelValidator]',
   providers: [{
       provide: NG_VALIDATORS,
-      useExisting: ServiceLevelDirective,
+      useClass: ServiceLevelDirective,
       multi: true
   }]
 })
 export class ServiceLevelDirective implements Validator {
 
   validate(c: AbstractControl): { [key: string]: boolean } | null {
-    console.log(c.value);
-    if (c.value !== undefined && (Number.isNaN(c.value) || c.value < 1 || c.value > 3)) {
-      return {
-        'serviceLevel': true
-      };
-    }
-    return null;
+    return checkServiceLevel(c, 1, 3);
   }
 }
