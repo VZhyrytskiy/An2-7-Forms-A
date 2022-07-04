244f483442998e1c76664afe539f0029be481f02
Vitaliy Zhyrytskyy <vitaliy_zhyrytskyy@epam.com>
2022-07-04 16:04:09 +0300

    fix

--- src/app/validators/custom.validators.ts
+++ src/app/validators/custom.validators.ts
@@ -16,8 +16,8 @@ export class CustomValidators {
   }
 
   static emailMatcher(c: AbstractControl): ValidationErrors | null {
-    const emailControl = c.get('email');
-    const emailConfirmControl = c.get('confirmEmail');
+    const emailControl = c.get('email')!;
+    const emailConfirmControl = c.get('confirmEmail')!;
 
     if (emailControl.pristine || emailConfirmControl.pristine) {
       return null;
